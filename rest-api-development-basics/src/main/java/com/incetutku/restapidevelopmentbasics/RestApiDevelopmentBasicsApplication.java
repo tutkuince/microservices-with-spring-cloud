1d2aa7b35dcaafc19c81c4e61223ef959e7b2b3d
Tutku Ince <tutku.ince@outlook.com>
2024-09-22 22:41:48 +0300

    initial commit for basics

---
+++ rest-api-development-basics/src/main/java/com/incetutku/restapidevelopmentbasics/RestApiDevelopmentBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.restapidevelopmentbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDevelopmentBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiDevelopmentBasicsApplication.class, args);
+    }
+
+}
---
+++ rest-api-development-basics/src/test/java/com/incetutku/restapidevelopmentbasics/RestApiDevelopmentBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.restapidevelopmentbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDevelopmentBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
