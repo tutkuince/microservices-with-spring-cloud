4380d00f5a503de490284ed7a3d86a8ad94eaa6f
Tutku Ince <tutku.ince@outlook.com>
2024-09-23 23:52:09 +0300

    Handle ResourceNotFoundException

--- rest-api-development-basics/src/main/java/com/incetutku/restapidevelopmentbasics/controller/UserController.java
+++ rest-api-development-basics/src/main/java/com/incetutku/restapidevelopmentbasics/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.incetutku.restapidevelopmentbasics.controller;
 
 import com.incetutku.restapidevelopmentbasics.dto.UserDTO;
+import com.incetutku.restapidevelopmentbasics.exception.ErrorDetails;
+import com.incetutku.restapidevelopmentbasics.exception.ResourceNotFoundException;
 import com.incetutku.restapidevelopmentbasics.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.View;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -13,9 +18,11 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final View error;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, View error) {
         this.userService = userService;
+        this.error = error;
     }
 
     @PostMapping
@@ -42,5 +49,16 @@ public class UserController {
     public ResponseEntity<String> deleteUserById(@PathVariable long id) {
         userService.deleteUserById(id);
         return ResponseEntity.ok("User successfully deleted");
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "USER_NOT_FOUND"
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 }
