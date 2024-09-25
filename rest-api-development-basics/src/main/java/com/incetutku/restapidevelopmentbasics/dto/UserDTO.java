051be119471e773d9fd12658aedd22408e1183b4
Tutku Ince <tutku.ince@outlook.com>
2024-09-25 19:21:28 +0300

    UserDTo swagger annotation

--- rest-api-development-basics/src/main/java/com/incetutku/restapidevelopmentbasics/dto/UserDTO.java
+++ rest-api-development-basics/src/main/java/com/incetutku/restapidevelopmentbasics/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.incetutku.restapidevelopmentbasics.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,16 +8,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "UserDTO Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
     private Long id;
+    @Schema(
+            description = "User Name"
+    )
     @NotEmpty(message = "User's Name should not be null or empty")
     private String name;
+    @Schema(
+            description = "User Surname"
+    )
     @NotEmpty(message = "User's Surname should not be null or empty")
     private String surname;
+    @Schema(
+            description = "User Email"
+    )
     @NotEmpty(message = "User's email should not be null or empty")
     @Email(message = "Email address should be valid")
     private String emailAddress;
